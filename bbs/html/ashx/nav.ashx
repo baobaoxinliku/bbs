f8190273c5ab4e3e9d69e801fc4ee565a28de7ca
baobaoxinliku <crazyxuc@vip.qq.com>
2016-05-22 14:36:39 +0800

    111

---
+++ bbs/html/ashx/nav.ashx
@@ -0,0 +1,1 @@
+﻿<%@ WebHandler Language="C#" CodeBehind="nav.ashx.cs" Class="bbs.html.ashx.nav" %>
---
+++ bbs/html/ashx/nav.ashx.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace bbs.html.ashx
+{
+    /// <summary>
+    /// nav 的摘要说明
+    /// </summary>
+    public class nav : IHttpHandler
+    {
+
+        public void ProcessRequest(HttpContext context)
+        {
+            context.Response.ContentType = "text/plain";
+            string action = context.Request.Form["Action"];
+            string json = "{}";
+            if (action == "Load")
+            {
+                if (context.Session["ID"] != null)
+                {
+                    json = "{\"info\":\"" + context.Session["Name"] + "\"}";
+                }
+
+            }
+
+        }
+
+        public bool IsReusable
+        {
+            get
+            {
+                return false;
+            }
+        }
+    }
+}
